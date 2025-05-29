7206d34591a8c5dbf484c0733b4f7c5c80f06f65
agg22 <amber.gorzynski22@imperial.ac.uk>
2025-05-29 13:31:53 +0100

    Rename flake to analyse_flake

---
+++ analyse_flake.py
@@ -0,0 +1,187 @@
+import pandas as pd
+import os
+import subprocess
+from pathlib import Path 
+
+def process_raw_results(raw_output : Path, test_output : Path, summary_output : Path):
+
+    with open(raw_output,'r') as f:
+        test_raw = f.readlines()
+
+    tests = [x for x in test_raw if ('- pass' in x or '- fail' in x or '- skip' in x)]
+
+    print(f'Writing test output to {test_output}')
+    with open(test_output,'w') as f:
+        f.writelines(tests)
+
+    test_summary = get_test_summary(test_raw)
+
+    print(f'Writing test summary to {test_summary}')
+    with open(summary_output, 'w') as f:
+        f.write(test_summary)
+
+def get_test_summary(test_output : list[str]) -> str:
+    summary = '\n'.join(test_output[-10:])
+    index = summary.find('Completed in')
+    if index != -1:
+        summary = summary[summary.find('Completed in'):]
+    else:
+        summary = 'Problem obtaining summary results. Check manually'
+    return summary
+
+def result_map(raw : str) -> str:
+    if ' - pass' in raw:
+        return 'pass'
+    if ' - fail' in raw:
+        return 'fail'
+    if ' - skip' in raw:
+        return 'skip'
+
+    print('Problem with result map!')
+    exit()
+
+def test_map(raw : str) -> str:
+    if ' - pass' in raw:
+        return raw[:raw.find(' - pass')]
+    if ' - fail' in raw:
+        return raw[:raw.find(' - fail')]
+    if ' - skip' in raw:
+        return raw[:raw.find(' - skip')]
+
+    print('Problem with test map!')
+    exit()
+
+def merge_run_results(output_tests : list[Path], output_name : str):
+
+    results = pd.DataFrame(columns=['test'])
+
+    for i, file in enumerate(output_tests):
+
+        print(f'File number {i}: {file}')
+
+        with open(file, 'r') as f:
+            raw = f.readlines()
+
+        if len(raw) == 0:
+            continue
+
+        print(f'len(raw) = {len(raw)}')
+
+        df = pd.DataFrame(raw, columns = [f'raw_{i}'])
+
+        df[f'result_{i}'] = df[f'raw_{i}'].apply(result_map)
+        df['test'] = df[f'raw_{i}'].apply(test_map)
+        print('\ndf:')
+        print(df.head(10))
+
+        results = results.merge(df, on = 'test', how = 'outer')
+
+        print('\nResults:')
+        print(results.head(10))
+
+    # Check that all tests are present in every run
+    nan_rows = results.isna().any(axis=1)
+    nan_results = nan_rows[nan_rows == True]
+    if len(nan_results > 0):
+        print('Tests missing from run!')
+        print(nan_results)
+        exit(1)
+    else:
+        print('No tests are missing from any run')
+
+    results['unique_results'] = list(map(set,results.filter(regex='result_*').values))
+    results['unique_results'] = results['unique_results'].apply(lambda x: len(x))
+
+    print(f'Writing all results to {output_name}')
+    print(results.head(10))
+    results.to_csv(output_name, sep='\t')
+
+    return results
+
+def find_flaky_tests(all_csv : Path, flaky_csv : Path) -> pd.DataFrame:
+
+    results = pd.read_csv(all_csv, sep = '\t')
+
+    flaky_tests = results[results['unique_results'] > 1]
+    cols = flaky_tests.columns
+    cols = [x for x in cols if ('raw' not in x)]
+    flaky_tests = flaky_tests[cols]
+
+    flaky_tests.to_csv(flaky_csv)
+
+def count_pass(x : pd.Series):
+    return len([i for i in x if i == 'pass'])
+
+def count_fail(x : pd.Series):
+    return len([i for i in x if i == 'fail'])
+
+def report_results(n_flakes,
+        n_runs,
+        n_tests,
+        n_tests_that_only_flake_once,
+        n_tests_that_flake_more_than_once):
+
+    print(f'\nReport at the most granular test level')
+    print(f'\nThe CTS has {n_tests} tests in total')
+    print(f'\nThere are {n_flakes} flaky tests across {n_runs} runs of the CTS')
+    print(f'\nThis is {n_flakes/n_tests}% of the total CTS tests')
+    print(f'\nOf these flaky tests, {n_tests_that_only_flake_once} only had one flake')
+    print('(i.e. one run in which their result differed from the result in all the other runs),')
+    print(f'while {n_tests_that_flake_more_than_once} flaked more than once.')
+    print('\n')
+
+def analyse(flaky_df, n_runs, n_tests):
+
+    n_flakes = len(flaky_df)
+    print(f'There are {n_flakes} flaky tests')
+    if n_flakes == 0:
+        exit()
+
+    # Get distribution of passes and fails
+    flaky_df['n_pass'] = flaky_df.filter(regex='result_*').apply(count_pass, axis=1)
+    flaky_df['n_fail'] = flaky_df.filter(regex='result_*').apply(count_fail, axis=1)
+
+    passes = flaky_df['n_pass'].value_counts()
+    fails = flaky_df['n_fail'].value_counts()
+
+    assert ((passes[1] + passes[n_runs - 1]) == (fails[1] + fails[n_runs - 1]))
+
+    n_tests_that_only_flake_once = passes[1] + passes[n_runs - 1]
+    n_tests_that_flake_more_than_once = n_flakes - n_tests_that_only_flake_once
+
+    # Report results
+    report_results(n_flakes, n_runs, n_tests, n_tests_that_only_flake_once, n_tests_that_flake_more_than_once)
+  
+def main():
+    n_runs = 10
+
+    base = Path('/home/ubuntu/dev')
+    output_dir = Path(base, 'data', 'cts_results', 'output_150525')
+    
+    flaky_csv = Path(output_dir, 'flaky_tests.csv')
+    all_csv = Path(output_dir, 'all_results.csv')
+
+    outputs_raw = [Path(output_dir, f'output_raw_{i}.txt') for i in range(n_runs)]
+    outputs_tests = [Path(output_dir, f'output_tests_{i}.txt') for i in range(n_runs)]
+    outputs_summary = [Path(output_dir, f'output_summary_{i}.txt') for i in range(n_runs)]
+
+    # Process raw results
+    for i in range(n_runs):
+        process_raw_results(outputs_raw[i], output_tests[i], output_summary[i])
+
+    merge_run_results(outputs_raw, all_csv)    
+
+    find_flaky_tests(all_csv, flaky_csv)
+
+    # Load processed results
+    flaky_df = pd.read_csv(flaky_csv, sep='\t')
+    all_df = pd.read_csv(all_csv, sep='\t')
+    
+    all_tests = all_df['test'].to_list()
+    n_tests = len(all_tests)
+
+    # Analyse results at most granular level (test x parameters)
+    analyse(flaky_df, n_runs, n_tests)
+
+if __name__=="__main__":
+    main()
